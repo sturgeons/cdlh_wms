41f35dc159e877be9b99735c336aabc88b3f58cb
yx392 <414328226@qq.com>
2025-05-06 13:46:02 +0800

    feat(cdlh_wms): 更新设置和表格样式
    - 在 settings.py 中添加了允许的主机列表。
    - 更新了 jssp/index.html 中表格的列标题和样式，增加了列宽和居中对齐，提升了表格的可读性。

--- cdlh_wms/settings.py
+++ cdlh_wms/settings.py
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS=['mgn.delink.top','8.149.129.48','10.238.238.249']
